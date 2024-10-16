2ecc7a735db50140e7506ded7c9273cc26e7af1e
Jonathan Narvaez Sosa <jonathan.narvaez@encora.com>
2024-10-16 13:28:38 -0600

    Initialize frontend with React and TypeScript

---
+++ src/main/java/com/spark/todoapp/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.spark.todoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Permite CORS para todas las rutas
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
